6ab6b64744ff617a1240a0034ce9bac6237036f1
Sophie-WH <31245747+Sophie-WH@users.noreply.github.com>
2018-01-23 10:05:08 -0600

    Create Assignment12.java

---
+++ Assignment12.java
@@ -0,0 +1,21 @@
+public class LetsaGo21
+{
+    public static void printAddress(String name, String address, String cityStateZip, String phone, String area)
+    {
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(cityStateZip);
+        System.out.println(phone);
+        System.out.println(area);
+        
+    }
+    public static void main(String[] args)
+    {
+        String name = "Chris Ortman";
+        String address = "742 Evergreen Terrace";
+        String cityStateZip = "Springfield, SA, 55555";
+        String phone = "555-867-5309";
+        String area = "CIS-121";
+        printAddress(name,address,cityStateZip,phone,area);
+    }
+}
